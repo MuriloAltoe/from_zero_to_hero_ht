41919efcb8e5495b1f1cc6aa4e862f3b262ad886
AltoeMurilo <muriloaleme17@gmail.com>
2024-03-23 12:53:54 -0300

    [upd]: montando a tela de exibição do produto

--- lib/features/products/presentation/product_detail_page.dart
+++ lib/features/products/presentation/product_detail_page.dart
@@ -18,11 +18,58 @@ class ProductDetail extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(product.title),
-            Text(product.brand),
-            Text(product.description),
-            Text(product.category),
-            Text(product.price.toStringAsFixed(2)),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    product.title,
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(product.brand),
+                      ),
+                      Text(
+                        product.rating.toStringAsFixed(1),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 200.0,
+              child: PageView.builder(
+                itemCount: product.images.length,
+                itemBuilder: (context, index) {
+                  return Image.network(
+                    product.images[index],
+                    fit: BoxFit.cover,
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 8), // Adiciona 8 pixels de espaço vertical
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(product.description),
+                  Text(product.category),
+                  Text(
+                    'US ${product.price.toStringAsFixed(2)}',
+                    style: const TextStyle(fontSize: 16),
+                    textAlign: TextAlign.right,
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
